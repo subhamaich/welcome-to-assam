97673b1ee5bb71c4f088c4574be848d172b6c36f
Subham Aich <subhamaich@hotmail.com>
2020-09-24 01:01:16 +0530

    add ip

--- app.js
+++ app.js
@@ -57,10 +57,10 @@ app.use("/images", campgroundRoutes);
 app.use("/images/:id/comments", commentRoutes);
 
 
-app.listen(3001, function(){
+//app.listen(3001, function(){
+//   console.log("The Server Has Started!");
+//});
+
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Server Has Started!");
 });
-
-//app.listen(process.env.PORT, process.env.IP, function(){
-//   console.log("The YelpCamp Server Has Started!");
-//});
